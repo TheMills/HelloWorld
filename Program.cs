e2f7d1831c497e8d72a6d7290d7f51e621c8f05a
Jan Møller <jan@themills.dk>
2018-06-17 19:57:05 +0200

    Smol change

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!\r\nFuck haters!");
+            Console.WriteLine("Get Bitches!");
         }
     }
 }
